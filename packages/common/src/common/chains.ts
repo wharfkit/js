69652c0a2386a7b56b359f676e1570641d68d3a4
Aaron Cox <aaron@greymass.com>
2023-07-28 14:46:49 -0700

    Adding a handful of explorers.

--- packages/common/src/common/chains.ts
+++ packages/common/src/common/chains.ts
@@ -65,6 +65,10 @@ export const Chains: Record<string, ChainDefinition> = {
     EOS: ChainDefinition.from({
         id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
         url: 'https://eos.greymass.com',
+        explorer: {
+            prefix: 'https://bloks.io/transaction/',
+            suffix: '',
+        },
     }),
     Jungle4: ChainDefinition.from({
         id: '73e4385a2708e6d7048834fbc1079f2fabb17b3c125b146af438971e90716c4d',
@@ -73,6 +77,10 @@ export const Chains: Record<string, ChainDefinition> = {
     Telos: ChainDefinition.from({
         id: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
         url: 'https://telos.greymass.com',
+        explorer: {
+            prefix: 'https://explorer.telos.net/transaction/',
+            suffix: '',
+        },
     }),
     TelosTestnet: ChainDefinition.from({
         id: '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f',
@@ -81,6 +89,10 @@ export const Chains: Record<string, ChainDefinition> = {
     WAX: ChainDefinition.from({
         id: '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4',
         url: 'https://wax.greymass.com',
+        explorer: {
+            prefix: 'https://waxblock.io/transaction/',
+            suffix: '',
+        },
     }),
     WAXTestnet: ChainDefinition.from({
         id: 'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12',
@@ -89,6 +101,10 @@ export const Chains: Record<string, ChainDefinition> = {
     UX: ChainDefinition.from({
         id: '8fc6dce7942189f842170de953932b1f66693ad3788f766e777b6f9d22335c02',
         url: 'https://api.uxnetwork.io',
+        explorer: {
+            prefix: 'https://explorer.uxnetwork.io/tx/',
+            suffix: '',
+        },
     }),
 }
 

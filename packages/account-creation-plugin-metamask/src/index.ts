03505c02859f53ce57d194962731fee63a27a6fb
dafuga <danielfugere28@gmail.com>
2024-07-25 17:45:41 -0700

    chore: implemented account creation

--- packages/account-creation-plugin-metamask/src/index.ts
+++ packages/account-creation-plugin-metamask/src/index.ts
@@ -1,65 +1,145 @@
+import {AccountCreator} from '@greymass/create-account'
 import {
     AbstractAccountCreationPlugin,
     AccountCreationPlugin,
     AccountCreationPluginConfig,
-    CreateAccountResponse,
-    Chains,
     CreateAccountContext,
+    Checksum256Type,
+    PublicKey,
+    Chains,
 } from '@wharfkit/session'
 import {AccountCreationPluginMetadata} from '@wharfkit/session'
+import {MetaMaskInpageProvider, RequestArguments} from '@metamask/providers'
+import {checkIsFlask, getSnapsProvider, InvokeSnapParams, Snap} from './metamask'
 
-export class AccountCreationPluginTEMPLATE
+export type GetSnapsResponse = Record<string, Snap>
+
+const DEFAULT_SNAP_ORIGIN = 'local:http://localhost:8080'
+const ACCOUNT_CREATION_SERVICE_URL =
+    'https://adding-login-through-apple.account-creation-portal.pages.dev/buy'
+
+export class AccountCreationPluginMetamask
     extends AbstractAccountCreationPlugin
     implements AccountCreationPlugin
 {
-    /**
-     * The logic configuration for the account-creation plugin.
-     */
+    public installedSnap: Snap | null = null
+    public provider: MetaMaskInpageProvider | null = null
+    public isFlask = false
+
     readonly config: AccountCreationPluginConfig = {
-        // Should the user interface display a chain selector?
         requiresChainSelect: true,
-
-        // Optionally specify if this plugin only works with specific blockchains.
-        // supportedChains: [Chains.Jungle4],
     }
-    /**
-     * The metadata for the account-creation plugin to be displayed in the user interface.
-     */
+
     readonly metadata: AccountCreationPluginMetadata = AccountCreationPluginMetadata.from({
-        name: 'Account Creation Plugin Template',
-        description: 'A template that can be used to build account creation plugins!',
-        logo: 'base_64_encoded_image',
-        homepage: 'https://someplace.com',
+        name: 'Account Creation Plugin Metamask',
+        description: 'Plugin to create EOS accounts using Metamask public key',
     })
-    /**
-     * A unique string identifier for this account-creation plugin.
-     *
-     * It's recommended this is all lower case, no spaces, and only URL-friendly special characters (dashes, underscores, etc)
-     */
+
     get id(): string {
-        return 'account-creation-plugin-template'
+        return 'account-creation-plugin-metamask'
     }
 
-    /**
-     * The name of the account-creation plugin to be displayed in the user interface.
-     */
     get name(): string {
-        return 'Account Creation Plugin Template'
+        return 'Account Creation Plugin Metamask'
     }
 
-    /**
-     * Performs the account creationg logic required to create the account.
-     *
-     * @param options CreateAccountContext
-     * @returns Promise<CreateAccountResponse>
-     */
-    // TODO: Remove these eslint rule modifiers when you are implementing this method.
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    async create(context: CreateAccountContext): Promise<CreateAccountResponse> {
-        // Example response...
-        return {
-            chain: Chains.EOS,
-            accountName: 'wharfkit1111',
+    async create(context: CreateAccountContext) {
+        if (context.chain?.id !== Chains.EOS.id) {
+            throw new Error('Only EOS is currently supported by this plugin.')
         }
+        const qs = new URLSearchParams()
+        qs.set('supported_chains', String(context.chain))
+        if (context.appName) {
+            qs.set('scope', String(context.appName))
+        }
+
+        const publicKey = await this.retrievePublicKey(context.chain.id)
+
+        qs.set('owner_key', String(publicKey))
+        qs.set('active_key', String(publicKey))
+        const accountCreator = new AccountCreator({
+            supportedChains: [String(context.chain?.id)],
+            fullCreationServiceUrl: `${ACCOUNT_CREATION_SERVICE_URL}?${qs.toString()}`,
+            scope: context.appName || 'Antelope App',
+        })
+        const accountCreationResponse = await accountCreator.createAccount()
+
+        console.log({accountCreationResponse})
+
+        if ('sa' in accountCreationResponse && 'sp' in accountCreationResponse) {
+            return {
+                accountName: accountCreationResponse.sa,
+                chain: Chains.EOS,
+            }
+        } else {
+            throw new Error(accountCreationResponse.error)
+        }
+    }
+
+    async initialize() {
+        if (!this.provider) {
+            this.provider = await getSnapsProvider()
+        }
+        if (this.provider && !this.installedSnap) {
+            this.isFlask = await checkIsFlask(this.provider)
+            await this.setSnap()
+            if (!this.installedSnap) {
+                await this.requestSnap()
+                if (this.installedSnap) {
+                    await this.initialize()
+                }
+            }
+        }
+    }
+
+    async retrievePublicKey(chainId: Checksum256Type): Promise<PublicKey> {
+        await this.initialize()
+        if (!this.provider) {
+            throw new Error('Metamask not found')
+        }
+        const result = (await this.invokeSnap({
+            method: 'antelope_getPublicKey',
+            params: {chainId: String(chainId)},
+        })) as string
+        return PublicKey.from(result)
+    }
+
+    async request({method, params}) {
+        if (!this.provider) {
+            throw new Error('Snap provider not found')
+        }
+        const data =
+            (await this.provider.request({
+                method,
+                params,
+            } as RequestArguments)) ?? null
+        return data
+    }
+
+    async setSnap() {
+        const snaps = (await this.request({
+            method: 'wallet_getSnaps',
+            params: {},
+        })) as GetSnapsResponse
+        this.installedSnap = snaps[DEFAULT_SNAP_ORIGIN] ?? null
+    }
+
+    async requestSnap(id?: string, version?: string) {
+        const snapId = id || DEFAULT_SNAP_ORIGIN
+        const snaps = (await this.request({
+            method: 'wallet_requestSnaps',
+            params: {
+                [snapId]: version ? {version} : {},
+            },
+        })) as Record<string, Snap>
+        this.installedSnap = snaps?.[snapId] ?? null
+    }
+
+    async invokeSnap({method, params}: InvokeSnapParams, id?: string) {
+        const snapId = id || DEFAULT_SNAP_ORIGIN
+        return this.request({
+            method: 'wallet_invokeSnap',
+            params: {snapId, request: {method, params}},
+        })
     }
 }

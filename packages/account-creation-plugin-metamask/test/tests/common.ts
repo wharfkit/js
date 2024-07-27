287665e0af40de97083712e237643d0b196bce0c
dafuga <danielfugere28@gmail.com>
2024-07-26 20:19:58 -0700

    chore: getting tests passing

--- packages/account-creation-plugin-metamask/test/tests/common.ts
+++ packages/account-creation-plugin-metamask/test/tests/common.ts
@@ -1,27 +1,22 @@
-import {assert} from 'chai'
-import {Chains, PermissionLevel, SessionKit} from '@wharfkit/session'
-import {
-    mockChainDefinition,
-    mockPermissionLevel,
-    mockSessionKitArgs,
-    mockSessionKitOptions,
-} from '@wharfkit/mock-data'
+import {Chains, SessionKit} from '@wharfkit/session'
+import {mockSessionKitArgs} from '@wharfkit/mock-data'
 
-import {AccountCreationPluginTEMPLATE} from '$lib'
+import {AccountCreationPluginMetamask} from '$lib'
+import {setupEthereumMock} from './mocks/ethereum'
 
-suite('AccountCreationPluginTEMPLATE', function () {
+suite('AccountCreationPluginMetamask', function () {
+    setup(function () {
+        setupEthereumMock() // Set up the Ethereum mock before each test
+    })
     test('createAccount', async function () {
-        // const kit = new SessionKit(
-        //     {
-        //         ...mockSessionKitArgs,
-        //         accountCreationPlugins: [new AccountCreationPluginTEMPLATE()],
-        //     },
-        //     mockSessionKitOptions
-        // )
+        const kit = new SessionKit(mockSessionKitArgs, {
+            accountCreationPlugins: [new AccountCreationPluginMetamask()],
+        })
+        // This will throw an error because we are not mocking the
+        // browser environment or the Metamask provider in this test
         // const result = await kit.createAccount({
-        //     chain: mockChainDefinition.id,
-        //     permissionLevel: mockPermissionLevel,
+        //     chain: Chains.EOS,
+        //     pluginId: 'account-creation-plugin-metamask',
         // })
-        // Add your own assertions here...
     })
 })

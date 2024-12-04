56757e2a5b26b6ad270582da9f16fb0686188fbb
dafuga <danielfugere28@gmail.com>
2024-12-04 12:44:52 -0800

    enhancement: using different owner and active key

--- packages/account-creation-plugin-metamask/src/index.ts
+++ packages/account-creation-plugin-metamask/src/index.ts
@@ -83,10 +83,10 @@ export class AccountCreationPluginMetamask
             qs.set('scope', String(context.appName))
         }
 
-        const publicKey = await this.retrievePublicKey(currentChain.id)
+        const {ownerPublicKey, activePublicKey} = await this.retrievePublicKeys(currentChain.id)
 
-        qs.set('owner_key', String(publicKey))
-        qs.set('active_key', String(publicKey))
+        qs.set('owner_key', String(ownerPublicKey))
+        qs.set('active_key', String(activePublicKey))
         const accountCreator = new AccountCreator({
             supportedChains: [String(currentChain.id)],
             fullCreationServiceUrl: `${this.accountCreationServiceUrl}?${qs.toString()}`,
@@ -135,16 +135,22 @@ export class AccountCreationPluginMetamask
         }
     }
 
-    async retrievePublicKey(chainId: Checksum256Type): Promise<PublicKey> {
+    async retrievePublicKeys(
+        chainId: Checksum256Type
+    ): Promise<{ownerPublicKey: string; activePublicKey: string}> {
         await this.initialize()
         if (!this.provider) {
             throw new Error('Metamask not found')
         }
-        const result = (await this.invokeSnap({
-            method: 'antelope_getPublicKey',
+        const ownerPublicKey = (await this.invokeSnap({
+            method: 'antelope_getOwnerPublicKey',
             params: {chainId: String(chainId)},
         })) as string
-        return PublicKey.from(result)
+        const activePublicKey = (await this.invokeSnap({
+            method: 'antelope_getActivePublicKey',
+            params: {chainId: String(chainId)},
+        })) as string
+        return {ownerPublicKey, activePublicKey}
     }
 
     async request({method, params}) {

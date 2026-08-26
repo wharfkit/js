a1362af5f9cf4db6e20509beeca3258ce05c181a
aaroncox <aaron.cox@greymass.com>
2026-08-25 21:42:33 -0700

    Rewrite rollup externals and repository field for @wharfkit/wallet-plugin-cloudwallet

--- packages/wallet-plugin-cloudwallet/rollup.config.js
+++ packages/wallet-plugin-cloudwallet/rollup.config.js
@@ -17,10 +17,7 @@ const banner = `
  */
 `.trim()
 
-const external = [
-    ...Object.keys(pkg.peerDependencies),
-    ...Object.keys(pkg.dependencies),
-]
+const external = Object.keys(pkg.dependencies)
 
 /** @type {import('rollup').RollupOptions} */
 export default [
